4534d9744ac8814c9d686e89e0279e25ab7652de
madhuriso <madhuri.padule@gmail.com>
2017-09-08 18:52:49 -0700

    blank commit

---
+++ src/main/java/com/suyash/app/App.java
@@ -0,0 +1,13 @@
+package com.suyash.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
