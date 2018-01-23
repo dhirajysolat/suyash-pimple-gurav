4443da0ac8f33d29d19336c1c89c355537a8d81c
madhuriSo <madhuri.padule@gmail.com>
2018-01-22 19:34:49 -0800

    Remove duplicate

--- src/test/java/com/suyash/app/SumTargerTest.java
+++ src/test/java/com/suyash/app/SumTargerTest.java
@@ -1,7 +1,22 @@
 package com.suyash.app;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 /**
  * Created by Madhuri on 9/29/17.
  */
 public class SumTargerTest {
+
+    @Test
+    public void checkTwoSum(){
+
+        int[] input={2,4,7,3};
+        int[] output={0,2};
+
+        SumTarget sumTarget= new SumTarget();
+        assertArrayEquals(output,sumTarget.twoSum(input,9));
+
+    }
 }
